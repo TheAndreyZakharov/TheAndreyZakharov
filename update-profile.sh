7f4736d298cb1c9568fc3c38b9ef2982f51bcef7
TheAndreyZakharov <andrey.connections@gmail.com>
2025-05-29 21:49:23 +0300

    файл для команд

---
+++ update-profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Генерация SVG..."
+npm run generate
+
+echo "Добавление файлов в git..."
+git add .
+
+echo "Коммит..."
+git commit -m "auto-update profile svg"
+
+echo "Пуш в GitHub..."
+git push origin main
+
+echo "✅ Готово!"
